1e8f61d224c8f36722151976883faa0c80221910
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-07-21 18:38:33 +0900

    Assignment3: Add ViewList and customed bottom navigation bar

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -11,53 +11,57 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              actions: [Icon(Icons.search),Icon(Icons.dehaze), Icon(Icons.notifications)],
-              leading: Text("금호동3가"),
-            ),
-            body: Container(
-              height: 150,
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children: [
-                  Image.asset('placeholder.png', width: 150,),
-                  Container(
-                    width: 300,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('카메라 팝니다', style: TextStyle(),),
-                        Text('금호동 3가'),
-                        Text('7000원'),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Icon(Icons.favorite_outline),
-                            Text('4')
-                          ],
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-            bottomNavigationBar: BottomAppBar(
-              child: SizedBox(
-                height: 70, // 원하는 높이로 설정
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(Icons.phone),
-                    Icon(Icons.message),
-                    Icon(Icons.contact_page)
-                  ],
-                ),
-              ),
-            )
+          appBar: AppBar(),
+          body: ListView(
+            children: [
+              UserList(),
+              UserList(),
+              UserList()
+            ],
+          ) ,
+          bottomNavigationBar: Footer(),
         )
     );
 
+  }
+}
+
+class Footer extends StatelessWidget {
+  const Footer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 100,
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Icon(Icons.phone),
+          Icon(Icons.message),
+          Icon(Icons.contact_page)
+        ],
+      ),
+    );
+  }
+}
+
+
+
+class UserList extends StatelessWidget {
+  const UserList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      child: Row(
+        children: [
+          Image.asset('placeholder.png'),
+          Text('홍길동')
+        ],
+      ),
+    );
   }
 }

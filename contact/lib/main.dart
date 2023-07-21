edbe50e77742d033bb937a5b21dbc6ed3020b1cd
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-07-21 18:17:59 +0900

    Modify: Modify assignment2

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -15,72 +15,33 @@ class MyApp extends StatelessWidget {
               actions: [Icon(Icons.search),Icon(Icons.dehaze), Icon(Icons.notifications)],
               leading: Text("금호동3가"),
             ),
-            body:Container( // 부모 위젯 (Container 등 다른 위젯을 사용해도 됨)
-              width: double.infinity, // Container의 너비를 원하는 값으로 설정 (예시로 300 지정)
-              color: Colors.white, // Container 배경색 (테스트를 위해 색상 지정)
-              height: 200,
+            body: Container(
+              height: 150,
+              padding: EdgeInsets.all(10),
               child: Row(
-                mainAxisSize: MainAxisSize.max, // Row가 부모의 가로 영역을 전부 차지하도록 설정
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
+                  Image.asset('placeholder.png', width: 150,),
                   Container(
-                    width: 200,
-                    height: 200,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('/placeholder.png'),
-                        fit: BoxFit.cover
-                      )
-                    ),
-                  ),
-                  Container(
-                    width: 450,
-                    height: 200,
-                    color: Colors.white,
+                    width: 300,
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        SizedBox(
-                          width: double.infinity,
-                          height: 100,
-                          child: Text("캐논 DSLR 100D (단렌즈, 충전기 16기가 SD 포함"),
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          height: 30,
-                          child: Text("성동구 행당동 끌올 10분 전"),
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          height: 30,
-                          child: Text("210,000원"),
-                        ),
-                        Container(
-                          width: double.infinity,
-                          height: 40,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                width: 30,
-                                height: 30,
-                                child: Icon(Icons.heart_broken),
-                              ),
-                              SizedBox(
-                                width: 30,
-                                height: 30,
-                                child: Center(child: Text("4", textAlign: TextAlign.center)),
-                              )
-                            ],
-                          ),
+                        Text('카메라 팝니다', style: TextStyle(),),
+                        Text('금호동 3가'),
+                        Text('7000원'),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(Icons.favorite_outline),
+                            Text('4')
+                          ],
                         )
                       ],
                     ),
-                  ),
+                  )
                 ],
               ),
-            )
-            ,
+            ),
             bottomNavigationBar: BottomAppBar(
               child: SizedBox(
                 height: 70, // 원하는 높이로 설정

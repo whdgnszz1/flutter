745a8a31883bb98a03500de980e8157840212f88
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-07-22 20:46:33 +0900

    Assignment4: Add infinite scroll feature

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './style.dart' as style;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -34,6 +35,12 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
+  addList(moreList){
+    setState(() {
+      list.add(moreList);
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -56,7 +63,7 @@ class _MyAppState extends State<MyApp> {
           )
           ],
       ),
-      body: [ Home(list: list), Text("샵페이지")][tab],
+      body: [ Home(list: list, addList: addList), Text("샵페이지")][tab],
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
@@ -76,32 +83,71 @@ class _MyAppState extends State<MyApp> {
 
 
 
-class Home extends StatelessWidget {
-  Home({Key? key, this.list}) : super(key: key);
-  final dynamic list;
+class Home extends StatefulWidget {
+  Home({Key? key, this.list, this.addList}) : super(key: key);
+  final list;
+  final addList;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var scroll = ScrollController();
+
+  getData() async {
+    try {
+      return await http.get(Uri.parse('https://codingapple1.github.io/app/more1.json'));
+    } catch (error) {
+      print("Error fetching data: $error");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    scroll.addListener(() async {
+      if(scroll.position.pixels == scroll.position.maxScrollExtent){
+        print("같음");
+        var data = await getData();
+        if (data != null) {
+          print(jsonDecode(data.body));
+          widget.addList(jsonDecode(data.body));
+          print(widget.list);
+        }
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    return ListView.builder(itemCount: list.length, itemBuilder: (c, i){
-      return Column(
-        children: [
-          Image.network(list[i]["image"]),
-          Container(
-            constraints: BoxConstraints(maxWidth: 600),
-            padding: EdgeInsets.all(20),
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    if (widget.list.isNotEmpty){
+      return ListView.builder(
+          itemCount: widget.list.length,
+          controller: scroll,
+          itemBuilder: (c, i){
+            return Column(
               children: [
-                Text(list[i]["likes"].toString()),
-                Text(list[i]["user"]),
-                Text(list[i]["content"]),
-              ],
-            ),
-          )
-        ],
-      );
-    });
+                Image.network(widget.list[i]["image"]),
+                Container(
+                  constraints: BoxConstraints(maxWidth: 600),
+                  padding: EdgeInsets.all(20),
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('좋아요 ${widget.list[i]["likes"]}'),
+                      Text(widget.list[i]["date"]),
+                      Text(widget.list[i]["content"]),
+                    ],
+                  ),
+            )
+          ],
+        );
+      });
+    } else {
+      return Text("로딩중임");
+    }
 
   }
 }

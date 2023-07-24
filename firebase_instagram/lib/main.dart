6795b651c58021f2baf3adc8f47527e29db9318f
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-07-25 01:26:41 +0900

    Feat: Connect ios to firebase

--- firebase_instagram/lib/main.dart
+++ firebase_instagram/lib/main.dart
@@ -3,8 +3,16 @@ import 'package:firebase_instagram/responsive/responsive_layout_screen.dart';
 import 'package:firebase_instagram/responsive/web_screen_layout.dart';
 import 'package:firebase_instagram/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 

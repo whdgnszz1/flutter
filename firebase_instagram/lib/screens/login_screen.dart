d95e1a41c468a7f3c98d68408a58aec4c2bb627a
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-07-25 11:49:16 +0900

    Design: Design login UI

--- firebase_instagram/lib/screens/login_screen.dart
+++ firebase_instagram/lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_instagram/utils/colors.dart';
+import 'package:firebase_instagram/widgets/text_field_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -10,6 +11,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +31,7 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Flexible(child: Container(), flex: 1,),
               // svg image
               SvgPicture.asset(
                 'assets/ic_instagram.svg',
@@ -28,11 +40,64 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               const SizedBox(height: 64),
               // text field input for email
-
+              TextFieldInput(
+                hintText: "Email",
+                textEditingController: _emailController,
+                textInputType: TextInputType.emailAddress,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
               // text field input for password
-
+              TextFieldInput(
+                hintText: "Password",
+                textEditingController: _passwordController,
+                textInputType: TextInputType.text,
+                isPass: true,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
               // button login
+              InkWell(
+                child: Container(
+                  child: const Text("Log in"),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4))
+                      ),
+                     color: Colors.blue,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Flexible(child: Container(), flex: 2,),
               // Transitioning to signing up
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: const Text("계정이 아직 없으신가요?"),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 8
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: (){},
+                    child: Container(
+                      child: const Text("회원가입", style: TextStyle(fontWeight: FontWeight.bold),),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),

145d0d49c047533dad606f0ce6045e7efca12f6f
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-07-25 14:43:04 +0900

    Feat: Add storage method to upload image

--- firebase_instagram/lib/resources/auth_methods.dart
+++ firebase_instagram/lib/resources/auth_methods.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_instagram/resources/storage_methods.dart';
 import 'package:flutter/material.dart';
 
 class AuthMethods {
@@ -24,6 +25,9 @@ class AuthMethods {
         UserCredential cred = await _auth.createUserWithEmailAndPassword(email: email, password: password);
 
         print(cred.user!.uid);
+        
+        String photoUrl = await StorageMethods().uploadImageToStorage('profilePics', file, false);
+        
         // add user to our database
         await _firestore.collection('users').doc(cred.user!.uid).set({
           'username': username,
@@ -32,6 +36,7 @@ class AuthMethods {
           "bio": bio,
           "followers": [],
           "following": [],
+          "photoUrl": photoUrl
         });
 
         //

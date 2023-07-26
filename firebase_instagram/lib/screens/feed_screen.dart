3ebcd54eb15878b85148347235efc8e1d1eda3e9
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-07-26 10:09:13 +0900

    Design: Design appbar

---
+++ firebase_instagram/lib/screens/feed_screen.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_instagram/utils/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        centerTitle: false,
+        title: SvgPicture.asset(
+          'assets/ic_instagram.svg',
+          color: primaryColor,
+          height: 32,
+        ),
+        actions: [
+          IconButton(onPressed: (){},
+              icon: const Icon(Icons.messenger_outline)
+          )
+        ],
+      ),
+      body: ,
+    );
+  }
+}
--- firebase_instagram/lib/utils/global_variable.dart
+++ firebase_instagram/lib/utils/global_variable.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_instagram/screens/add_post_screen.dart';
+import 'package:firebase_instagram/screens/feed_screen.dart';
 import 'package:flutter/material.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
-  Text('feed'),
+  FeedScreen(),
   Text('search'),
   AddPostScreen(),
   Text('notif'),

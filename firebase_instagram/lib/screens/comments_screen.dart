f81757fd33b8a6804c7e055a2f7363f181c48b70
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-07-26 13:56:40 +0900

    Design: Design comments screen bottom nav

--- firebase_instagram/lib/screens/comments_screen.dart
+++ firebase_instagram/lib/screens/comments_screen.dart
@@ -17,6 +17,42 @@ class _CommentsScreenState extends State<CommentsScreen> {
         title: const Text('Comments'),
         centerTitle: false,
       ),
+      body: CommentCard(),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: kToolbarHeight,
+          margin: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          padding: const EdgeInsets.only(left: 16, right:8),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/placeholder.png'),
+                radius: 18,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 8),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Comment as username',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  child: const  Text('작성하기', style: TextStyle(
+                    color: blueColor,
+                  ),),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
